04f5863ff02b08da9513dbc2e3e1bb14fd744253
evjeesm <jekas.obps@gmail.com>
2024-11-11 19:32:45 +0200

    Tool for controlling submodule operations

--- build.sh
+++ build.sh
@@ -1,4 +1,38 @@
 #!/bin/sh
 
-CFLAGS='-std=gnu18 -Wall -Wextra -Wno-override-init -Werror'
-gcc -g -O0 $CFLAGS -Ideps -Ldeps tifc.c canvas.c display.c input.c frame.c -lcircbuf_static -lhashmap_static -lsparse_static -ldynarr_static -lvector_static -lm
+CFLAGS="
+    -g
+    -O0
+    -std=gnu18
+    -Wall
+    -Wextra
+    -Wno-override-init
+    -Werror
+"
+
+CPPFLAGS="
+    -Ldeps
+    -Ideps
+    -Iui
+    -I.
+"
+
+OBJECTS="
+    tifc.c
+    canvas.c
+    display.c
+    input.c
+    frame.c
+"
+
+LIBS="
+    -lcircbuf_static
+    -lhashmap_static
+    -lsparse_static
+    -ldynarr_static
+    -lvector_static
+    -lm
+"
+
+gcc $CPPFLAGS $CFLAGS $OBJECTS $LIBS
+
--- rebuild-modules.sh
+++ rebuild-modules.sh
@@ -1,8 +1,52 @@
 #!/bin/sh
 
-CFLAGS="-O0 -g -Wall -Wextra -Wno-override-init"
+THREADS=8
+CFLAGS="-g -O0 -Wall -Wextra -Werror -Wno-override-init -Wno-unused-function"
 
-git submodule update --init --recursive --remote
-git submodule foreach './autogen.sh; make distclean; ./configure && make -j8'
+HELP='List of sub-commands:\n
+\t update      \t  Recusively init/update submodules.\n
+\t remove-all  \t  Remove submodules and clear folders.\n
+\t fresh     \t\t  Configure and generate build files (prior `update` invokation required).\n
+\t config      \t  Configure and clean compiled artifacts (maybe usefull if `CFLAGS` changed).\n
+\t remake      \t  Clean all build files and then make.\n
+\t make      \t\t  Just make. (if source code of submodule changed)\n
+'
 
-
+[ $# = 0 ] && { echo $HELP; exit 0; }
+[ $# -ge 1 ] && {
+    case $1 in
+        update)
+            git submodule update --init --recursive --remote
+            [ $? -ne 0 ] && exit $? || exit 0
+        ;;
+        remove-all)
+            echo Removing submodules!
+            git submodule deinit --force --all
+            [ $? -ne 0 ] && exit $? || exit 0
+        ;;
+        fresh)
+            echo Fresh ...
+            git submodule foreach "make -j$THREADS distclean; ./autogen.sh && ./configure"
+            [ $? -ne 0 ] && exit $? || exit 0
+        ;;
+        config)
+            echo Configuring ...
+            git submodule foreach "./configure CFLAGS=\"$CFLAGS\" && make -j$THREADS clean"
+            [ $? -ne 0 ] && exit $? || exit 0
+        ;;
+        remake)
+            echo Clean Remaking ...
+            git submodule foreach "make -j$THREADS clean; make -j$THREADS"
+            [ $? -ne 0 ] && exit $? || exit 0
+        ;;
+        make)
+            echo Making ...
+            git submodule foreach "make -j$THREADS"
+            [ $? -ne 0 ] && exit $? || exit 0
+        ;;
+        *)
+            echo Unknown sub-command!
+            exit 1
+        ;;
+    esac
+}
